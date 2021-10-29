88d892bef28207056421323550d1437efa216636
Abdo Abosamra <67236632+AbdoAbosamra@users.noreply.github.com>
2021-10-29 15:35:49 +0200

    finish Modles

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },  # end of dictionary
-    {   # start of dictionary
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-
-]
+from .models import Post
 
 def home(request):
+    posts = Post.objects.all()
     context = { 'posts':posts }
     return render(request, 'blog/home.html' ,context)
 
