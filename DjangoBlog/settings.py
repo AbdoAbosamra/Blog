2816558bb952f029efce78e111c353d062fd9db7
Abdo Abosamra <67236632+AbdoAbosamra@users.noreply.github.com>
2021-11-01 00:58:26 +0200

    Authentication and regsiteration

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'home'
--- users/views.py
+++ users/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisrationForm
 
 def register(request):
     if request.method =='POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisrationForm(request.POST)
         if form.is_valid():
+            form.save() # save user to database
             username = form.cleaned_data.get('username') # cleaned_data is a dictionary
-            messages.success(request, f'Account Created for {username}') # f string
-            return redirect('home')
+            messages.success(request, f'Your Account has been Created for! You now able to login') # f string
+            return redirect('login')
         else:
-            print(messages.error(request, 'Error'))
+            messages.error(request, 'Error')
     else:
-        form = UserCreationForm()   # To show the form without request.
+        form = UserRegisrationForm()   # To show the form without request.
         
     return render(request, 'users/register.html' , {'form' : form})
