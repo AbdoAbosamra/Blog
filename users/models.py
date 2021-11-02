f29bfe386f4494c577f83a9ce01be2da75790490
Abdo Abosamra <67236632+AbdoAbosamra@users.noreply.github.com>
2021-11-02 18:09:18 +0200

    some improvements

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # OneToOneField is a link between two models
+    image =models.ImageField(default='a.jpg', upload_to = 'profile_pics') # upload_to is the path where the image will be saved
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
--- users/views.py
+++ users/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisrationForm
-
+from .forms import *
+from .models import Profile
+from django.http import HttpResponse
 def register(request):
     if request.method =='POST':
         form = UserRegisrationForm(request.POST)
@@ -20,4 +21,22 @@ def register(request):
     return render(request, 'users/register.html' , {'form' : form})
 @login_required
 def profile(request):
-    return render(request , 'users/profile.html')
+    #Profile.objects.get_or_create(user=request.user) # get_or_create is a method of model
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
+
+    context ={ 
+        'u_form':u_form,
+        'p_form':p_form,
+        
+    }
+    
+    return render(request , 'users/profile.html' , context)
+'''''
+def fixme(request):
+    users = User.objects.all()
+    for user in users:
+        obj, created = Profile.objects.get_or_create(user=user)
+        print(user.username,' : ',created)
+    print("all done")
+    return HttpResponse("It's done.")'''
